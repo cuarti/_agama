467c43a419217914536d88c5c735268a8aa2372e
cuarti <cuarti1992@gmail.com>
2016-11-30 23:01:28 +0100

    Added has, add and remove methods to Collection interface

--- modules/core/src/collections/Collection.ts
+++ modules/core/src/collections/Collection.ts
@@ -4,6 +4,12 @@ import {Iterator} from './Iterator';
 
 export interface Collection<K, V> extends Iterator<K, V> {
 
+    has(key: K): boolean;
+
     get(key: K): V;
+
+    add(key: K, value: V): void;
+
+    remove(key: K): void;
 
 }
